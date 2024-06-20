ddd3b1c9d1921d5688093e9852e2c42516d4a4e6
Treedeo <treedeo@yahoo.com>
2024-06-20 14:16:39 +0300

    Update script.js

--- script.js
+++ script.js
@@ -4,17 +4,22 @@ document.addEventListener('DOMContentLoaded', () => {
     const adminButton = document.getElementById('admin-button');
     const adminLoginModal = document.getElementById('admin-login-modal');
     const adminLoginForm = document.getElementById('admin-login-form');
+    const toggleVisibilityButton = document.getElementById('toggle-visibility');
     let pinCode = '';
     let isAdminLogin = false;
+    let isPinVisible = false;
 
     pinPadButtons.forEach(button => {
         button.addEventListener('click', () => {
             if (button.id === 'clear') {
                 pinCode = '';
+            } else if (button.id === 'toggle-visibility') {
+                isPinVisible = !isPinVisible;
+                pinCodeDisplay.textContent = isPinVisible ? pinCode : '*'.repeat(pinCode.length);
             } else {
                 pinCode += button.textContent;
             }
-            pinCodeDisplay.textContent = pinCode;
+            pinCodeDisplay.textContent = isPinVisible ? pinCode : '*'.repeat(pinCode.length);
             if (isAdminLogin && pinCode.length === 6) {
                 if (pinCode === '250883') {
                     window.location.href = 'admin.html'; // Переход на страницу администратора
